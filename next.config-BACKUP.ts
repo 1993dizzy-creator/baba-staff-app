754c9b7c92d97b688e574b39ecd499e2ed6f4ee3
1993dizzy-creator <1993dizzy@gmail.com>
2026-04-09 00:25:20 +0700

    restore next config

--- next.config-BACKUP.ts
+++ next.config-BACKUP.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
+
+// 타입시크릿에서 배포막힐경우 이걸로 뚫고 에러로그 확인
